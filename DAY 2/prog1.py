df0fe12a41a101c4f460d84f8311a1cbac58a410
Shreya Badve <71018084+Apollo-bot35@users.noreply.github.com>
2024-06-29 00:22:42 +0530

    Add files via upload

---
+++ DAY 2/prog1.py
@@ -0,0 +1,4 @@
+arr=[3,4,5,6,7]
+new_num=int(input("Enter the number :"))
+arr.append(new_num)
+print(arr)
---
+++ DAY 2/prog2.py
@@ -0,0 +1,6 @@
+arr=[]
+limit=int(input("Enter the size of array"))
+for i in range(0,limit):
+    ele=int(input("Enter the number :"))
+    arr.append(ele)
+print(arr)

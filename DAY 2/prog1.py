be0705335e089dfe238e50b35a7e440ed22dfbc6
Shreya Badve <71018084+Apollo-bot35@users.noreply.github.com>
2024-06-29 00:28:41 +0530

    Delete DAY 2 directory

--- DAY 2/prog1.py
+++
@@ -1,4 +0,0 @@
-arr=[3,4,5,6,7]
-new_num=int(input("Enter the number :"))
-arr.append(new_num)
-print(arr)
--- DAY 2/prog2.py
+++
@@ -1,6 +0,0 @@
-arr=[]
-limit=int(input("Enter the size of array"))
-for i in range(0,limit):
-    ele=int(input("Enter the number :"))
-    arr.append(ele)
-print(arr)
